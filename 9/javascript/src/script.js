41f4467e78b34a99189e807bb248e2540826d962
narineVardanyan1998 <116202601+narineVardanyan1998@users.noreply.github.com>
2024-02-01 14:19:59 +0400

    Add files via upload
    მე-9 დავალება

---
+++ 9/javascript/src/script.js
@@ -0,0 +1,56 @@
+
+// let numbers = [15, 53, 22, 198, 10, 28, 16, 70, 33, 951];
+// let maxNumber = numbers[0];
+
+
+// for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] > maxNumber) {
+//         maxNumber = numbers[i];
+//     }
+// }
+
+// console.log(`ყველაზე დიდი რიცხვი: ${maxNumber}`);
+
+
+
+
+// let myArray = [10, 20, 30, 40, 50];
+
+// for (let i = 0; i < myArray.length; i++) {
+//     console.log(myArray[i]);
+// }
+
+
+
+function shefasebisfunc(x) {
+  if (x > 50) {
+    console.log("ძვირია");
+  } else if (x >= 20 && x <= 50) {
+    console.log("საშუალო ფასი");
+  } else {
+    console.log(" იაფია");
+  }
+}
+
+function display(x) {
+  x(55);
+}
+
+
+display(shefasebisfunc);
+
+
+// // Function to be passed as a parameter
+// executeFunction(greet);
+// function greet(name) {
+//   console.log("Hello, " + name + "!");
+// }
+
+// // Function that takes another function as a parameter
+// function executeFunction(parameter) {
+//  greet();
+// }
+
+// // Usage
+
+
