135fcc5551fc91ae7c0e1cf93206d1791dcdff97
narineVardanyan1998 <116202601+narineVardanyan1998@users.noreply.github.com>
2024-02-09 14:36:12 +0400

    Add files via upload
     მე-11 დავალება

---
+++ 4/data.js
@@ -0,0 +1,177 @@
+   const products = [
+      {
+        "id": 1,
+        "name": "Smartphone",
+        "price": 1400,
+        "image" : [
+          {
+            "alt" : "Apple",
+            "url": "https://drop.ndtv.com/TECH/product_database/images/3222016120930AM_635_apple_iphone_se.jpeg"
+          }
+        ],
+        "categories": [
+          {
+            "categoryId": 101,
+            "categoryName": "Electronics"
+          },
+          {
+            "categoryId": 102,
+            "categoryName": "Smartphones"
+          },
+        ],
+        "details": {
+          "description": "Latest model with high-end specs.",
+          "specs": [
+            {
+              "specName": "Screen",
+              "specValue": "6.5 inch OLED"
+            },
+            {
+              "specName": "Battery",
+              "specValue": "4500 mAh"
+            }
+          ]
+        }
+      },
+      {
+        "id": 2,
+        "name": "Watch",
+        "price": 80000.00,
+        "image" : [
+          {
+            "alt" : "Rolex",
+            "url": "https://www.orientalwatch.com/images/rolex/watch_assets_front_facing/landscape_assets/M228238-0006_modelpage_front_facing_landscape.webp"
+          }
+        ],
+        "categories": [
+          {
+            "categoryId": 101,
+            "categoryName": "Electronics"
+          },
+          {
+            "categoryId": 104,
+            "categoryName": "Models"
+          }
+        ],
+        "details": {
+          "description": "Powerful laptop suitable for both work and play.",
+          "specs": [
+            {
+              "specName": "CPU",
+              "specValue": "Intel i7"
+            },
+            {
+              "specName": "RAM",
+              "specValue": "16GB"
+            }
+          ]
+        }
+      },
+      {
+        "id": 3,
+        "name": "Laptop",
+        "price": 2400.00,
+        "image" : [
+          {
+            "alt" : "ASUS",
+            "url": "https://m.media-amazon.com/images/I/61+r3+JstZL.jpg"
+          },
+
+        ],
+        "categories": [
+          {
+            "categoryId": 101,
+            "categoryName": "Electronics"
+          },
+          {
+            "categoryId": 104,
+            "categoryName": "Computers"
+          }
+        ],
+        "details": {
+          "description": "Powerful laptop suitable for both work and play.",
+          "specs": [
+            {
+              "specName": "CPU",
+              "specValue": "Intel i7"
+            },
+            {
+              "specName": "RAM",
+              "specValue": "16GB"
+            }
+          ]
+        }
+      },
+      {
+        "id": 3,
+        "name": "TV",
+        "price": 5500.00,
+        "image" : [
+          {
+            "alt" : "Samsung SMART TV",
+            "url": "https://s3.zoommer.ge/zoommer-images/thumbs/0166504_samsung-tv-ue85au7172ux-eu_550.jpeg"
+          },
+
+        ],
+        "categories": [
+          {
+            "categoryId": 101,
+            "categoryName": "Electronics"
+          },
+          {
+            "categoryId": 104,
+            "categoryName": "TV"
+          }
+        ],
+        "details": {
+          "description": "Powerful laptop suitable for both work and play.",
+          "specs": [
+            {
+              "specName": "CPU",
+              "specValue": "Intel i7"
+            },
+            {
+              "specName": "RAM",
+              "specValue": "16GB"
+            }
+          ]
+        }
+      },
+      {
+        "id": 3,
+        "name": "TV",
+        "price": 3500.00,
+        "image" : [
+          {
+            "alt" : "VIDAA SMART TV",
+            "url": "https://www.sencor.com/Sencor/media/content/product/35059333-02.jpg"
+          },
+
+        ],
+        "categories": [
+          {
+            "categoryId": 101,
+            "categoryName": "Electronics"
+          },
+          {
+            "categoryId": 104,
+            "categoryName": "TV"
+          }
+        ],
+        "details": {
+          "description": "Powerful laptop suitable for both work and play.",
+          "specs": [
+            {
+              "specName": "CPU",
+              "specValue": "Intel i7"
+            },
+            {
+              "specName": "RAM",
+              "specValue": "16GB"
+            }
+          ]
+        }
+      },
+    ]
+  
+export default products;
